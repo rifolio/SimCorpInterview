4069defd551e167f14605b425dea8d0a95f07e9f
rifo <horbatenko.vladislav@gmail.com>
2025-08-12 02:57:43 +0200

    Random txt file generator for tests

---
+++ generate_file.py
@@ -0,0 +1,16 @@
+import os
+import random
+import string
+
+target_size_mb = 20
+file_path = f"text_files/random_text_{target_size_mb}mb.txt"
+target_size_bytes = target_size_mb * 1024 * 1024
+
+with open(file_path, "w", encoding="utf-8") as f:
+    written = 0
+    chunk_size = 1024 * 1024
+    while written < target_size_bytes:
+        words = ["".join(random.choices(string.ascii_letters + " ", k=10)) for _ in range(chunk_size // 11)]
+        text_chunk = " ".join(words)
+        f.write(text_chunk)
+        written += len(text_chunk.encode("utf-8"))
